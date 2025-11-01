5791befe176fcf72dbafb7528bdd7be4cb90a47c
messi <itken1001@gmail.com>
2025-11-01 15:56:56 +0800

    20251101

---
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -0,0 +1,23 @@
+package vn.hoidanit.laptopshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import vn.hoidanit.laptopshop.service.UserService;
+
+@Controller
+public class UserController {
+
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @RequestMapping("/")
+    public String getHomepage() {
+        String tesst = userService.hendleHello();
+        return "hello";
+    }
+
+}
